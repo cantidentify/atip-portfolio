3c7d67dfcb6f75f5af6896f576eea6a01c0e2c2e
AtipDev <yo.atip_st@tni.ac.th>
2022-11-27 17:45:00 +0700

    modify on home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,17 @@ import { Fragment } from "react"
 
 const Home = () => {
     return(
-        <section className="container">
-            <div className="introduce">
-                <div>
-                    <div>I'm Atip Yongstar</div>
+        <section className="intro-container">
+            <div className="introduce-content">
+                <div className="introduce-content-left">
+                    <div>I'm <span className="my-name">Atip Yongstar</span></div>
                     <div>I learn from people how to work better.</div>
-                    <div className="resume-btn">Button Resume</div>
+                    <div className="resume-btn">
+                        <button class="button-78" role="button">RESUME</button>
+                    </div>
+
                 </div>
-                <div>
+                <div className="introduce-content-right">
                     Image
                 </div>
             </div>
