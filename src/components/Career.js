dc753e69d7b54cb655bd1c71eb6222ec33535a52
AtipDev <yo.atip_st@tni.ac.th>
2024-04-05 18:25:20 +0700

    Modify career description

--- src/components/Career.js
+++ src/components/Career.js
@@ -7,25 +7,29 @@ const Career = () => {
         <div className="career-content">
             <div className="career-detail">
                 <p className="career-title">Software Engineer</p>
-                <p className="career-ferm-name">T.N. Incorporation | 2022 June - Present</p>
+                <p className="career-ferm-name">T.N. Digital Solutions Co., Ltd. | 2022 June - Present</p>
                 <ul className="career-list">
-                    <li>Working in a role of Golang Backend Engineer to develop banking system in an agile environment</li>
-                    <li>Developed a microservices-based architecture with Docker, Kubernetes, YottaDB</li>
+                    <li>Working in a role of Golang Backend Engineer to develop banking system in an agile environment.</li>
+                    <li>Developed a microservices-based architecture with Docker, Kubernetes and YottaDB primarily serves as a database.</li>
+                    <li>Responsibility for design, develop, and maintain core banking software solutions tailored to the specific needs of financial institutions, including retail and commercial banks.</li>
+                    <li>We work with a culture of open communication is fostered, where team members are encouraged to share ideas, provide feedback, and raise concerns openly. </li>
                 </ul>
             </div>
             <div className="career-detail">
                 <p className="career-title">Programmer</p>
                 <p className="career-ferm-name">Innova Software Co., Ltd. | 2021 June - 2022 March</p>
                 <ul className="career-list">
-                    <li>Programmer(C#) as a Japanese team to build and customize software for Japan company</li>
+                    <li>Develop high-quality software solutions using C# programming language, adhering to the project requirements and coding standards.</li>
+                    <li>Familiarize yourself with Japanese work culture, including communication protocols, business etiquette, and project management practices, to effectively collaborate with the client and meet their expectations.</li>
                 </ul>
             </div>
             <div className="career-detail">
                 <p className="career-title">Back-end developper <small>Internship</small></p>
                 <p className="career-ferm-name">Optimized Solution | June - September 2020</p>
                 <ul className="career-list">
-                    <li>IT equipment stock system on web base developing team</li>
-                    <li>Technical support</li>
+                    <li>Mostly focus on development of IT equipment stock system on web base developing using PHP language and xampp web stack and mySQL as a database</li>
+                    <li>Work collaboratively with team members to achieve common objectives. Contribute all expertise and skills to collective problem-solving efforts.</li>
+                    <li>Provide prompt and courteous assistance to customers experiencing technical issues with products or services. This may include troubleshooting software, hardware, or network-related problems.</li>
                 </ul>
             </div>
         </div>
